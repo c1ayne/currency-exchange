05e6fafdc56e7297eb45526934b955bcc00d13a1
clayne <savva665544332211@yandex.ru>
2025-02-21 00:30:50 +0300

    create: template of Currency api

--- src/main/java/currency/CurrencyDAO.java
+++ src/main/java/currency/CurrencyDAO.java
@@ -1,4 +1,20 @@
 package currency;
 
+import util.classes.Currency;
+
+import java.util.ArrayList;
+
 public class CurrencyDAO {
+
+    public ArrayList<Currency> getAllCurrencies() {
+        return new ArrayList<Currency>();
+    }
+
+    public Currency getCurrency() {
+        return new Currency();
+    }
+
+    public Currency setCurrency() {
+        return new Currency();
+    }
 }
