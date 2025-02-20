e49363c648cb15db36505a17fcc2b34cb9cb109d
clayne <savva665544332211@yandex.ru>
2025-02-21 00:30:50 +0300

    create: template of Currency api

--- src/main/java/currency/CurrencyServlet.java
+++ src/main/java/currency/CurrencyServlet.java
@@ -1,4 +1,25 @@
 package currency;
 
-public class CurrencyServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/currency/*", "/currencies"})
+public class CurrencyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+
+    }
 }
