1e23d1199d80b5145b242bf6588cec65c0624ec2
clayne <savva665544332211@yandex.ru>
2025-02-22 03:25:10 +0300

    create: func for get all currencies

--- src/main/java/currency/CurrencyDAO.java
+++ src/main/java/currency/CurrencyDAO.java
@@ -2,19 +2,57 @@ package currency;
 
 import util.classes.Currency;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class CurrencyDAO {
 
+    String url = "jdbc:postgresql://localhost:5432/currency-exchange"; //DB url
+    String name = "postgres"; //DB login
+    String password = "root"; //DB password
+
+
     public ArrayList<Currency> getAllCurrencies() {
+
+        ArrayList<Currency> currencies = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection(url, name, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM currencies");
+
+
+            while (resultSet.next()) {
+                currencies.add(new Currency(
+                        resultSet.getInt("ID"),
+                        resultSet.getString("code"),
+                        resultSet.getString("fullname"),
+                        resultSet.getString("sign")
+                ));
+
+                connection.close();
+
+                return currencies;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return new ArrayList<Currency>();
     }
 
     public Currency getCurrency() {
-        return new Currency();
+        return null;
     }
 
     public Currency setCurrency() {
-        return new Currency();
+        return new Currency(2 , " ", " ", " ");
     }
 }
