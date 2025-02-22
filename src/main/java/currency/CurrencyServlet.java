4cf0a6c266c9d3f0348e82c9efcbc68475b4fee5
clayne <savva665544332211@yandex.ru>
2025-02-22 03:25:10 +0300

    create: func for get all currencies

--- src/main/java/currency/CurrencyServlet.java
+++ src/main/java/currency/CurrencyServlet.java
@@ -5,15 +5,38 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import util.classes.Currency;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+//@WebServlet("/currencies")
 @WebServlet(urlPatterns = {"/currency/*", "/currencies"})
 public class CurrencyServlet extends HttpServlet {
+
+    CurrencyDAO currencyDAO = new CurrencyDAO();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        PrintWriter writer = response.getWriter();
+
+        String servletPath = request.getServletPath();
+        String pathInfo = request.getPathInfo();
+        if ("/currencies".equals(servletPath)) {
+            Currency currency = currencyDAO.getCurrency();
+//            writer.println();
+//            writer.println();
+//            writer.println();
+//            writer.println();
+
+            System.out.println(currency.getId());
+            System.out.println(currency.getCode());
+            System.out.println(currency.getFullName());
+            System.out.println(currency.getSign());
+        }
+
 
     }
 
