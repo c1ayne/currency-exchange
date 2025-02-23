f980a923af11ee6fb972b03cc3f6b61bb3cfe7a9
clayne <savva665544332211@yandex.ru>
2025-02-23 21:36:58 +0300

    add: environment variables

--- src/main/java/currency/CurrencyDAO.java
+++ src/main/java/currency/CurrencyDAO.java
@@ -7,9 +7,18 @@ import java.util.ArrayList;
 
 public class CurrencyDAO {
 
-    String url = "jdbc:postgresql://localhost:5432/currency-exchange"; //DB url
-    String name = "postgres"; //DB login
-    String password = "root"; //DB password
+    String url;
+    String name;
+    String password;
+
+    public CurrencyDAO() {
+        this.url = System.getenv("DB_URL");
+        this.name = System.getenv("DB_LOGIN");
+        this.password = System.getenv("DB_PASSWORD");
+
+        if (url == null || name == null || password == null)
+            throw new RuntimeException("Не заданы переменные окружения для подключения к БД");
+    }
 
 
     public ArrayList<Currency> getAllCurrencies() {
--- src/main/java/exchange/ExchangeDAO.java
+++ src/main/java/exchange/ExchangeDAO.java
@@ -1,4 +1,36 @@
 package exchange;
 
+import util.classes.Exchange;
+
+import java.util.ArrayList;
+
 public class ExchangeDAO {
+    String url;
+    String name;
+    String password;
+
+    public ExchangeDAO() {
+        this.url = System.getenv("DB_URL");
+        this.name = System.getenv("DB_LOGIN");
+        this.password = System.getenv("DB_PASSWORD");
+
+        if (url == null || name == null || password == null)
+            throw new RuntimeException("Не заданы переменные окружения для подключения к БД");
+    }
+
+    public ArrayList<Exchange> getAllExchangeRates() {
+        return null;
+    }
+
+    public Exchange getExchangeRate() {
+        return null;
+    }
+
+    public Exchange setExchangeRate() {
+        return null;
+    }
+
+    public Exchange updateExchangeRate() {
+        return null;
+    }
 }
